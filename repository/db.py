882463462480e2e1676760f6b3ea8f4569ed2801
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-28 17:08:11 +0100

    feat: mockear sacar datos base datos

---
+++ repository/db.py
@@ -0,0 +1,10 @@
+from repository.stock import stock
+
+class DB():
+
+    @staticmethod
+    def get_stock():
+        return stock
+    @staticmethod
+    def get_item(name):
+        return [item for item in stock if item["name"] == name]
