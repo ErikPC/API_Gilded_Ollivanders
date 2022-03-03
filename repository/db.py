f4369ef8330aa900c09db3852a34c682a8ba560e
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-03 01:25:12 +0100

    feat: crear metodos para filtrar por sellIn y quality

--- repository/db.py
+++ repository/db.py
@@ -1,3 +1,4 @@
+from turtle import st
 from repository.stock import stock
 
 
@@ -9,3 +10,11 @@ class DB:
     @staticmethod
     def get_item(name):
         return [item for item in stock if item["name"] == name]
+    
+    @staticmethod
+    def get_sell_in(sell_in):
+        return [item for item in stock if item["sell_in"] == int(sell_in)]
+        
+    @staticmethod
+    def get_quality(quality):
+        return [item for item in stock if item["quality"] == int(quality)]
