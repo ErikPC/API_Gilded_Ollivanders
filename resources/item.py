969a3fae1e44169a3c57d2f2d30fdd82d6557b42
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-05 20:51:09 +0100

    fix: cambiar el codigo para que funciones con mongo Atlas

--- resources/item.py
+++ resources/item.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
-from repository.db import DB
+from repository.db_atlas import DB_atlas
 
 
 class Item(Resource):
     def get(self, name):
-        return DB.get_item(name)
+        return DB_atlas.get_item(name)
