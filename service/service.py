879a3b4ee5d7b3cb1a2f922e2f7484844c14a88e
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-07 19:06:09 +0100

    feat: añadir get_sell_in a Service

--- service/service.py
+++ service/service.py
@@ -5,3 +5,7 @@ class Service:
     @staticmethod
     def get_stock():
         return DB_atlas.get_stock()
+
+    @staticmethod
+    def get_sell_in(sell_in):
+        return DB_atlas.get_sell_in(sell_in)
