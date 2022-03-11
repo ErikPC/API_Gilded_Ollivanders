df33df6e53a807eb54cc654799d20d1b6b2169ce
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-11 13:41:30 +0100

    feat: añadir metodo update_item sin nada

--- resources/update_sotck.py
+++ resources/update_sotck.py
@@ -1,7 +1,14 @@
+from unittest import case
 from flask_restful import Resource
+from domain.Aged_brie import AgedBrie
 from service.service import Service
 
 
 class Update_stock(Resource):
     def get():
+        stock = Service.get_stock()
+        for item in stock:
+            if item["name"] == "Aged_brie":
+                AgedBrie.updateQuality()
+
         return Service.get_stock()
