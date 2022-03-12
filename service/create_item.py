59d42f461dc548a853f7d1b4fe54c972f74a3f6c
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-12 20:11:47 +0100

    refactor: añadir más items

--- service/create_item.py
+++ service/create_item.py
@@ -1,10 +1,21 @@
 from domain.Aged_brie import AgedBrie
 from domain.Conjured import Conjured
+from domain.Backstage import Backstage
+from domain.NormalItem import NormalItem
+from domain.Sulfuras import Sulfuras
 
 
-def create_item(name, quality=0, sell_in=0):
-    if name == "Aged_brie":
+def create_item(item):
+    name = item["name"]
+    quality = item["quality"]
+    sell_in = item["sell_in"]
+    if name == "Aged-brie":
         return AgedBrie(name, quality, sell_in)
     elif name == "Conjured":
-
         return Conjured(name, quality, sell_in)
+    elif name == "Backstage":
+        return Backstage(name, quality, sell_in)
+    elif name == "Normal-item":
+        return NormalItem(name, quality, sell_in)
+    else:
+        return Sulfuras(name, quality, sell_in)
