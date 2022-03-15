f25d11325c70e700f66e5f6bbf6782c909aa7f64
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-15 15:17:34 +0100

    test: cambiar orden de quality sell_in

--- test/test_conjured.py
+++ test/test_conjured.py
@@ -1,35 +1,39 @@
-from src.Conjured import Conjured
+from domain.Conjured import Conjured
 import pytest
+
 
 @pytest.mark.update_quality_conjured
 def test_update_quality_conjured():
-    conjured = Conjured("Conjured Mana Cake", 3, 6)
+    conjured = Conjured("Conjured Mana Cake", 6, 3)
     conjured.updateQuality()
     assert 2 == conjured.getSell_in()
     assert 4 == conjured.getQuality()
 
+
 @pytest.mark.update_quality_conjured_just_expired
 def test_update_quality_conjured_just_expired():
-    conjured = Conjured("Conjured Mana Cake", 0, 6)
+    conjured = Conjured("Conjured Mana Cake", 6, 0)
     conjured.updateQuality()
     assert -1 == conjured.getSell_in()
     assert 2 == conjured.getQuality()
 
+
 @pytest.mark.update_quality_conjured_expired
 def test_update_quality_conjured_expiredU():
-    conjured = Conjured("Conjured Mana Cake", -1, 6)
+    conjured = Conjured("Conjured Mana Cake", 6, -1)
     conjured.updateQuality()
     assert -2 == conjured.getSell_in()
     assert 2 == conjured.getQuality()
 
+
 @pytest.mark.quality_min_ZERO
-def test_quality_min_ZERO():    
+def test_quality_min_ZERO():
     brie = Conjured("Conjured Mana Cake", 1, 1)
     brie.updateQuality()
     assert 0 == brie.getSell_in()
     assert 0 == brie.getQuality()
 
-    brie = Conjured("Conjured Mana Cake", -1, 0)
+    brie = Conjured("Conjured Mana Cake", 0, -1)
     brie.updateQuality()
     assert -2 == brie.getSell_in()
     assert 0 == brie.getQuality()
--- test/test_normal_item.py
+++ test/test_normal_item.py
@@ -1,24 +1,26 @@
-from src.NormalItem import NormalItem
+from domain.NormalItem import NormalItem
 import pytest
 
 
 @pytest.mark.update_quality_normal_item
 def test_update_quality_normal_item():
-    normal = NormalItem("+5 Dexterity Vest", 10, 20)
+    normal = NormalItem("+5 Dexterity Vest", 20, 10)
     normal.updateQuality()
     assert 9 == normal.getSell_in()
     assert 19 == normal.getQuality()
 
+
 @pytest.mark.update_quality_normal_item_expired
 def test_update_quality_normal_item_expired():
-    normal = NormalItem("+5 Dexterity Vest", 0, 20)
+    normal = NormalItem("+5 Dexterity Vest", 20, 0)
     normal.updateQuality()
     assert -1 == normal.getSell_in()
     assert 18 == normal.getQuality()
 
+
 @pytest.mark.quality_min_ZERO
 def test_quality_min_ZERO():
-    normal = NormalItem("+5 Dexterity Vest", 10, 0)
+    normal = NormalItem("+5 Dexterity Vest", 0, 10)
     normal.updateQuality()
     assert 9 == normal.getSell_in()
     assert 0 == normal.getQuality()
