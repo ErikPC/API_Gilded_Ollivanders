de54dc6cda3ef36b7cfcc28d714657c906276385
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-22 18:18:22 +0100

    test: modificar nombres funciones

--- test/controller/test_get_item.py
+++ test/controller/test_get_item.py
@@ -1,43 +1,32 @@
-import pytest
-from factory import create_app
-
-create_app()
-
-
-@pytest.fixture
-def client():
-    return create_app().test_client()
-
-
-def test_aged_brie(client):
+def test_get_aged_brie(client):
     resp = client.get("/item/Aged-brie")
     assert resp.status_code == 200
     assert resp.is_json
     assert resp.json == [{"name": "Aged-brie", "quality": 8, "sell_in": 5}]
 
 
-def test_backstage(client):
+def test_get_backstage(client):
     resp = client.get("/item/Backstage")
     assert resp.status_code == 200
     assert resp.is_json
     assert resp.json == [{"name": "Backstage", "quality": 8, "sell_in": 5}]
 
 
-def test_normal_item(client):
+def test_get_normal_item(client):
     resp = client.get("/item/Normal-item")
     assert resp.status_code == 200
     assert resp.is_json
     assert resp.json == [{"name": "Normal-item", "quality": 8, "sell_in": 5}]
 
 
-def test_conjured(client):
+def test_get_conjured(client):
     resp = client.get("/item/Conjured")
     assert resp.status_code == 200
     assert resp.is_json
     assert resp.json == [{"name": "Conjured", "quality": 8, "sell_in": 5}]
 
 
-def test_sulfuras(client):
+def test_get_sulfuras(client):
     resp = client.get("/item/Sulfuras")
     assert resp.status_code == 200
     assert resp.is_json
