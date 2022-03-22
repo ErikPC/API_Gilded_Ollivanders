e52982edb29643f1cd29ebb99a4b98ac07b42f99
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-22 18:16:13 +0100

    test: añadir tests delete items

---
+++ test/controller/test_delete_item.py
@@ -0,0 +1,48 @@
+def test_delete_aged_brie(client):
+    resp = client.delete("/item/Aged-brie/8/5")
+    assert resp.status_code == 200
+    assert resp.is_json
+    assert resp.json == {
+        "Eliminado": True,
+        "Item": {"name": "Aged-brie", "quality": "8", "sell_in": "5"},
+    }
+
+
+def test_delete_backstage(client):
+    resp = client.delete("/item/Backstage/8/5")
+    assert resp.status_code == 200
+    assert resp.is_json
+    assert resp.json == {
+        "Eliminado": True,
+        "Item": {"name": "Backstage", "quality": "8", "sell_in": "5"},
+    }
+
+
+def test_delete_normal_item(client):
+    resp = client.delete("/item/Normal-item/8/5")
+    assert resp.status_code == 200
+    assert resp.is_json
+    assert resp.json == {
+        "Eliminado": True,
+        "Item": {"name": "Normal-item", "quality": "8", "sell_in": "5"},
+    }
+
+
+def test_delete_conjured(client):
+    resp = client.delete("/item/Conjured/8/5")
+    assert resp.status_code == 200
+    assert resp.is_json
+    assert resp.json == {
+        "Eliminado": True,
+        "Item": {"name": "Conjured", "quality": "8", "sell_in": "5"},
+    }
+
+
+def test_delete_sulfuras(client):
+    resp = client.delete("/item/Sulfuras/80/0")
+    assert resp.status_code == 200
+    assert resp.is_json
+    assert resp.json == {
+        "Eliminado": True,
+        "Item": {"name": "Sulfuras", "quality": "80", "sell_in": "0"},
+    }
